37bc4c22d274c36038f369f30b4f0bd85784b999
Pawan Dubey <pawanadubey@gmail.com>
2018-03-07 18:49:27 +0530

    Added splash activity

--- app/src/main/java/com/wordpress/pawanthepro/quickie/LauncherActivity.java
+++ app/src/main/java/com/wordpress/pawanthepro/quickie/LauncherActivity.java
@@ -1,9 +1,22 @@
 package com.wordpress.pawanthepro.quickie;
 
+import android.*;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -11,20 +24,31 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        new WaitForSomeTime().execute();
     }
 
-    private class WaitForSomeTime extends AsyncTask<String, String, String>{
+    private class WaitForSomeTime extends AsyncTask<String, String, String> {
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+
             startActivity(new Intent(LauncherActivity.this, MainActivity.class));
+            overridePendingTransition(R.anim.anim_slide_in_left,
+                    R.anim.anim_slide_out_left);
+            finish();
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
 
         @Override
         protected String doInBackground(String... params) {
             try {
-                Thread.sleep(30000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
